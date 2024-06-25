2d9499b9d5916cfd581799c2bd38b04e8fcee586
1dlvb <matushkin-2017@mail.ru>
2024-06-25 23:15:25 +0500

    feat: add method logging annotation functionality

---
+++ audit-lib-spring-boot-starter/src/main/java/org/lib/advice/LogLevel.java
@@ -0,0 +1,25 @@
+package org.lib.advice;
+
+/**
+ * Enum for all log levels
+ * Possible levels:
+ * {@link #DEBUG},
+ * {@link #ERROR},
+ * {@link #INFO},
+ * {@link #WARN},
+ * {@link #FATAL},
+ * {@link #OFF},
+ * {@link #TRACE},
+ * @author Matushkin Anton
+ */
+public enum LogLevel {
+
+    DEBUG,
+    ERROR,
+    INFO,
+    WARN,
+    FATAL,
+    OFF,
+    TRACE
+
+}
---
+++ audit-lib-spring-boot-starter/src/main/java/org/lib/appender/CustomFileAppender.java
@@ -0,0 +1,69 @@
+package org.lib.appender;
+
+import org.apache.logging.log4j.core.Filter;
+import org.apache.logging.log4j.core.Layout;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.appender.AbstractAppender;
+import org.apache.logging.log4j.core.config.Property;
+import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.apache.logging.log4j.core.config.plugins.PluginAttribute;
+import org.apache.logging.log4j.core.config.plugins.PluginElement;
+import org.apache.logging.log4j.core.config.plugins.PluginFactory;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.Serializable;
+
+/**
+ * Custom appender for logging to a file.
+ * @author Matushkin Anton
+ */
+@Plugin(name = "CustomFileAppender", category = "Core", elementType = "appender", printObject = true)
+public class CustomFileAppender extends AbstractAppender {
+
+    private String path;
+
+    protected CustomFileAppender(String name, Filter filter, Layout<? extends Serializable> layout, boolean ignoreExceptions, Property[] properties) {
+        super(name, filter, layout, ignoreExceptions, properties);
+    }
+
+    /**
+     * Provides output to a file
+     * @param event event to output
+     */
+    @Override
+    public void append(LogEvent event) {
+        final byte[] bytes = getLayout().toByteArray(event);
+        try (FileOutputStream outputStream = new FileOutputStream(path, true)) {
+            outputStream.write(bytes);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * @param path path for log file.
+     */
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    /**
+     * @param name name of an appender
+     * @param filter filters for appender
+     * @param layout layout for appender
+     * @param ignoreExceptions true to ignore, false to not ingonre
+     * @param properties properties for appender
+     * @return new {@link CustomFileAppender}
+     */
+    @PluginFactory
+    public static CustomFileAppender createAppender(
+            @PluginAttribute("name") String name,
+            @PluginElement("Filters") Filter filter,
+            @PluginElement("Layout") Layout<? extends Serializable> layout,
+            @PluginAttribute("ignoreExceptions") boolean ignoreExceptions,
+            @PluginElement("Properties") Property[] properties) {
+        return new CustomFileAppender(name, filter, layout, ignoreExceptions, properties);
+    }
+
+}
