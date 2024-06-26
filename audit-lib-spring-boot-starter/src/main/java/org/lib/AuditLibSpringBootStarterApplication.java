561c91a43c1eb92d2bfa448bd769cfef04f16954
1dlvb <matushkin-2017@mail.ru>
2024-06-27 00:27:56 +0500

    feat: add application file for starter

---
+++ audit-lib-spring-boot-starter/src/main/java/org/lib/AuditLibSpringBootStarterApplication.java
@@ -0,0 +1,14 @@
+package org.lib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
+public class AuditLibSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuditLibSpringBootStarterApplication.class, args);
+    }
+
+}
