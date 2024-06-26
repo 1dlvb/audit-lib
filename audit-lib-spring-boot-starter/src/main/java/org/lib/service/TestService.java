fcd021aa90a32314b102ee94b29ca3246963abf3
1dlvb <matushkin-2017@mail.ru>
2024-06-27 00:26:16 +0500

    test: add service for tests

---
+++ audit-lib-spring-boot-starter/src/main/java/org/lib/service/TestService.java
@@ -0,0 +1,23 @@
+package org.lib.service;
+
+import org.lib.advice.AuditLog;
+import org.lib.advice.LogLevel;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+    @AuditLog(logLevel = LogLevel.INFO)
+    public String performAction(String action) {
+        return action;
+    }
+
+    @AuditLog
+    public Double calculateAvg(Integer a, Integer b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Possible zero division!");
+        }
+        return (double) a / b;
+    }
+
+}
