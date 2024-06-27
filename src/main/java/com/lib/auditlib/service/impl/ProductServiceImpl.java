030dcc015730f024122dccfa3d6641a7d2092272
1dlvb <matushkin-2017@mail.ru>
2024-06-27 09:42:11 +0500

    refactor: remove unused files

--- src/main/java/com/lib/auditlib/service/impl/ProductServiceImpl.java
+++ src/main/java/com/lib/auditlib/service/impl/ProductServiceImpl.java
@@ -2,15 +2,22 @@ package com.lib.auditlib.service.impl;
 
 import com.lib.auditlib.service.ProductService;
 import org.lib.advice.AuditLog;
+import org.lib.advice.LogLevel;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductServiceImpl implements ProductService {
 
     @Override
+    @AuditLog(logLevel = LogLevel.INFO)
+    public String getProduct(String product) {
+        return product;
+    }
+
+    @Override
     @AuditLog
-    public String getProduct() {
-        return "Product";
+    public Double calculateAvg(Integer a, Integer b) {
+        return (double) a / b;
     }
 
 }
