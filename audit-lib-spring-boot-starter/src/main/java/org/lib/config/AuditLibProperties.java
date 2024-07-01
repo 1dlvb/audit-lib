3dcd97219973873dd9fb77a2169ab79b28f99f84
1dlvb <matushkin-2017@mail.ru>
2024-07-01 22:58:55 +0500

    refactor: add properties fields to a AuditLibProperties.java

--- audit-lib-spring-boot-starter/src/main/java/org/lib/config/AuditLibProperties.java
+++ audit-lib-spring-boot-starter/src/main/java/org/lib/config/AuditLibProperties.java
@@ -1,11 +1,26 @@
 package org.lib.config;
 
+import jakarta.annotation.PostConstruct;
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
  * Class for handling properties for annotation
  * @author Matushkin Anton
  */
+@Data
 @ConfigurationProperties(prefix = "audit-lib-spring-boot-starter")
 public class AuditLibProperties {
+
+    private boolean consoleEnabled;
+
+    private boolean fileEnabled;
+
+    private String filePath;
+
+    @PostConstruct
+    private void init() {
+        System.out.println("props init");
+    }
+
 }
