e477171f8bbe0763e17676fec13ae20740d72ccc
1dlvb <matushkin-2017@mail.ru>
2024-08-06 22:08:45 +0500

    chore: initial commit

---
+++ message-receiver/src/main/java/com/onedlvb/messagereceiver/MessageReceiverApplication.java
@@ -0,0 +1,14 @@
+package com.onedlvb.messagereceiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
+public class MessageReceiverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageReceiverApplication.class, args);
+    }
+
+}
